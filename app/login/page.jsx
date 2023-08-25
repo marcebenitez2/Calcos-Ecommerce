6970c7e2e1e49437f0a332f8e5fbe0fea95539f5
Marce Benitez <106833298+marcebenitez2@users.noreply.github.com>
2023-08-25 20:08:18 -0300

    Correccion redireccionamiento

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,29 +1,64 @@
-"use client"
-import login from '@/components/loginLogic';
-import React, { useEffect, useState } from 'react';
+"use client";
+import app from "@/firebase/firebase";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 const Page = () => {
 
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const router = useRouter()
 
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        login()
+  console.log(router)
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(""); // Corrección aquí
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const auth = getAuth();
+    try {
+      await signInWithEmailAndPassword(auth, username, password);
+      console.log("Entraste!!!");
+      router.push("/admin")
+    } catch (error) {
+      setErrorMessage("Credenciales incorrectas"); // Corrección aquí
     }
+  };
 
-    return (
-        <div className='w-full h-full flex items-center justify-center'>
-            <div className='bg-[#ffde59]  h-80 rounded-xl flex flex-col py-5 gap-8 md:w-2/6 w-3/6'>
-                <h1 className='text-center text-3xl font-semibold text-[#1f1f1f]'>LOGIN</h1>
-                <form className='flex flex-col w-full items-center gap-4'>
-                    <input className='bg-[#1f1f1f] w-3/5 h-8 px-3 rounded-md' placeholder='Nombre de usuario' required onChange={(e)=>setUsername(e.target.value)}/>
-                    <input className='bg-[#1f1f1f] w-3/5 h-8 px-3 rounded-md' placeholder='Contraseña' required onChange={(e)=>setPassword(e.target.value)}/>
-                    <button className='w-1/3 bg-[#1f1f1f] h-16 rounded-md' onClick={(e)=>handleSubmit(e)}>Iniciar sesion</button>
-                </form>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <div className="bg-[#ffde59]  h-80 rounded-xl flex flex-col py-5 gap-8 md:w-2/6 w-3/6">
+        <h1 className="text-center text-3xl font-semibold text-[#1f1f1f]">
+          LOGIN
+        </h1>
+        <form className="flex flex-col w-full items-center gap-4">
+          <input
+            className="bg-[#1f1f1f] w-3/5 h-8 px-3 rounded-md"
+            placeholder="Nombre de usuario"
+            required
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            className="bg-[#1f1f1f] w-3/5 h-8 px-3 rounded-md"
+            placeholder="Contraseña"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            className="w-1/3 bg-[#1f1f1f] h-16 rounded-md"
+            onClick={(e) => handleSubmit(e)}
+          >
+            Iniciar sesión
+          </button>
+          {errorMessage && (
+            <p className="text-red-500">{errorMessage}</p>
+          )}
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Page;
---
+++ firebase/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2lVdQcTgjrczj4Sah8iMLEdAuC9SSGSo",
+  authDomain: "blessed-calcos.firebaseapp.com",
+  projectId: "blessed-calcos",
+  storageBucket: "blessed-calcos.appspot.com",
+  messagingSenderId: "426540625149",
+  appId: "1:426540625149:web:4045ef21425a23abd2e294"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
+
+// firebase login
+
+//firebase init
+
+//firebase deploy
